b76ce7b104ea11950e0988127ab3db1ffb948e60
fyto <cristofher.ambiado@valoralabs.com>
2021-10-13 00:03:09 -0300

    loading view implementada...por verificar llamado ajax

---
+++ Martina.API/wwwroot/js/Cares/Create.js
@@ -0,0 +1,37 @@
+﻿'use strict';
+
+$(document).ready(async function () {
+
+
+    $("#btnCreate").click(function ()
+    {
+        $(".ovelay, .popup").fadeIn();
+
+        CallActionCreate();
+    });
+
+    function LoadingBar()
+    {
+        $("#fade-area").removeAttr("style");
+        $("#myModal").removeAttr("style");
+    }
+
+
+    function CallActionCreate()
+    {
+        $.ajax({
+            type: "POST",
+            url: '@Url.Action("Cares","Create")',
+            success: function () {
+                console.log("success modal");
+                setTimeout(function () { LoadingBar() }, 5000);
+            },
+            error: function () {
+                console.log("error modal");
+                setTimeout(function () { LoadingBar() }, 5000);
+            }
+        });
+
+    }
+
+});
